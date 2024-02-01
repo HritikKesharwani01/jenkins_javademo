7162b29742518ea6ebe0001d158ec85b3f020d93
HritikKesharwani01 <hritikkesharwani673@gmail.com>
2024-02-01 21:38:59 +0530

    Hello

--- App1.java
+++ App1.java
@@ -1,7 +1,9 @@
 public class App1{
 
     public static void main(String[] args){
-        
+        System.out.println("Java App Pulled & Build by Jenkins");
+        System.out.println("Congrats!");
+        System.out.println("Congrats");
         System.out.println("Java App Pulled & Build by Jenkins");
     }
 }
